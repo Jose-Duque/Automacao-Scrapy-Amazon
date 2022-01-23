2a6efaa2350d68dec0b7ae264d5d132b10c89102
José Carlos Duque Filho <cj_duque@hotmail.com>
2022-01-23 13:11:56 -0300

    created function data_save and finish progran

--- AmazonProduct/produtos_amazon.py
+++ AmazonProduct/produtos_amazon.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,17 +7,22 @@ from selenium.webdriver.support.ui import WebDriverWait as wdw
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 __author__ = 'José Carlos Duque Filho'
 __email__ = 'joseduquefilho03@gmail.com'
 
 TIMEOUT_DEFAULT = 10
 URL = 'https://www.amazon.com.br/'
+description = []
+price = []
 
-browser = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('__lang=pr-BR')
+chrome_options.add_argument('__disable-notifications')
+browser = webdriver.Chrome(options=chrome_options)
 
 def search():
     browser.get(URL)
+    browser.maximize_window()
     wdw(browser, TIMEOUT_DEFAULT).until(
         EC.visibility_of_element_located((By.ID, 'twotabsearchtextbox'))
     )
@@ -24,20 +30,35 @@ def search():
     pesquisar.send_keys('iphone', Keys.ENTER)
 
 def get_product():
-
+        global description
+        global price
         soup = BeautifulSoup(browser.page_source, 'html.parser')
 
         result = soup.find_all('div', {'data-component-type':'s-search-result'})
+        print(len(result))
         for x in range(len(result)):
             try:
                 item = result[x]
                 atag = item.h2.a
-                descricao = atag.text.strip()
+                descriptions = item.find('span', 'a-size-base-plus a-color-base a-text-normal').text
+                description.append(descriptions)
                 price_parent = item.find('span', 'a-price')
-                preco = price_parent.find('span', 'a-offscreen')
-                print(descricao, preco.text.strip())
+                prices = price_parent.find('span', 'a-offscreen').text
+                price.append(prices)
+
             except Exception as e:
-                print(e)
-search()
-get_product()
-# browser.quit()
+                price.append('sem preço')
+        browser.quit()
+
+def data_save():
+    data = {'Descrição do Produto': description, 'Preço': price}
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df = df.transpose()
+    with pd.ExcelWriter('Produtos.xlsx') as path:
+        df.to_excel(path, index=False, encoding='utf-8')
+
+if __name__ == '__main__':
+    search()
+    get_product()
+    data_save()
+    print('Programa executado com sucesso!!!')
